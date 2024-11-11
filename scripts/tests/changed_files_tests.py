702923d988d5cec6361641deba6c64d25a5d66d2
eddie4k <edwardoc@buffalo.edu>
2024-11-11 16:14:35 -0500

    Add tests

---
+++ scripts/tests/changed_files_tests.py
@@ -0,0 +1,27 @@
+import unittest
+from scripts.changed_files import changed_files
+
+
+class TestGetChangedFiles(unittest.TestCase):
+
+    def test_no_files(self):
+        self.assertCountEqual(changed_files.get_changed_files([]), [])
+
+    def test_no_matching_services(self):
+        files = ["other-service/file.txt", "another-service/anotherfile.txt"]
+        self.assertCountEqual(changed_files.get_changed_files(files), [])
+
+    def test_some_matching_services(self):
+        files = ["auth-service/file1.txt", "order-service/file2.txt", "unrelated/file3.txt"]
+        self.assertCountEqual(changed_files.get_changed_files(files), ["auth-service", "order-service"])
+
+    def test_all_matching_services(self):
+        files = ["auth-service/file1.txt", "sneaker-service/file2.txt", "order-service/file3.txt"]
+        self.assertCountEqual(changed_files.get_changed_files(files), ["auth-service", "sneaker-service", "order-service"])
+
+    def test_duplicate_services(self):
+        files = ["auth-service/file1.txt", "auth-service/file2.txt"]
+        self.assertCountEqual(changed_files.get_changed_files(files), ["auth-service"])
+
+if __name__ == "__main__":
+    unittest.main()
